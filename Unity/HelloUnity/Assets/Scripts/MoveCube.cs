63e4d14ed84fc23cb470e6976814048c7b936811
MohanLi <lmh19920411@163.com>
2016-01-31 08:59:03 +0800

    add

---
+++ Unity/HelloUnity/Assets/Scripts/MoveCube.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCube : MonoBehaviour {
+
+    private float speed;
+    void Start() {
+        speed = 1f; 
+    }
+
+    void Update() {
+        Move();
+    }
+
+    void Move() {
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }else if (Input.GetKey(KeyCode.S)) {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }else if (Input.GetKey(KeyCode.D)) {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        Debug.Log(Time.deltaTime);
+    }
+}
