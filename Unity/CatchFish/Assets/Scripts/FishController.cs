a43b1c56fccaf9017b2da08a914db42be8c4e5f6
MohanLi <lmh19920411@163.com>
2016-03-05 09:48:10 +0800

    添加鱼的游动

---
+++ Unity/CatchFish/Assets/Scripts/FishController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishController : MonoBehaviour {
+    //改变方向的时间
+    private float mDirectionTime = 1.5f;
+    //当前的时间
+    private float mCurrentTime = 0f;
+    //鱼游动的速度
+    private float mSpeed = 1f;
+    //鱼改变方向范围值(-angle, angle)
+    private float angle = 10f;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    //鱼游动
+    void MoveFish() {
+        transform.Translate(Vector3.right * Time.deltaTime * mSpeed);
+    }
+    //改变鱼的方向
+    void AjustDirection() {
+        transform.Rotate(Vector3.back, Random.Range(-angle, angle));
+    }
+
+	// Update is called once per frame
+    void FixedUpdate() {
+        if (mCurrentTime >= mDirectionTime) {
+            mCurrentTime = 0f;
+            AjustDirection();
+        }
+        mCurrentTime += Time.deltaTime;
+        MoveFish();
+       
+	}
+}
---
+++ Unity/CatchFish/Assets/Scripts/FishFactory.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishFactory : MonoBehaviour
+{
+    //创建鱼的时间
+    private float mCreateTime;
+    //
+    private float mCurrentTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        InitTime();
+    }
+
+    //初始化时间
+    void InitTime()
+    {
+        mCreateTime = Random.Range(1, 3);
+        mCurrentTime = 0f;
+    }
+
+    //创建鱼
+    void CreateFish()
+    {
+        GameObject fish = (GameObject)Instantiate(Resources.Load("fish_00"));
+        fish.transform.position = new Vector3(-8, Random.Range(-4, 4), 0);
+
+        //为鱼添加组件
+        fish.AddComponent<DestroyObject>();
+        fish.AddComponent<FishController>();
+    }
+
+    void FixedUpdate()
+    {
+        if (mCurrentTime >= mCreateTime)
+        {
+            CreateFish();
+            InitTime();
+        }
+        mCurrentTime += Time.deltaTime;
+    }
+}
+
