9a8413b1a178fe5fa184f8ed6f450c2092d42bda
MohanLi <lmh19920411@163.com>
2016-04-06 07:21:10 +0800

    modified

---
+++ Unity/MyRPG/Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+    public Transform[] mBackground;
+
+    private Transform mCamera;
+    private Vector3 mPrePosition;
+
+	// Use this for initialization
+	void Start () {
+        mCamera = Camera.main.transform;
+        mPrePosition = mCamera.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float posX = mCamera.position.x - mPrePosition.x;
+	}
+}
