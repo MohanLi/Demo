80ef63f198689834af4c2a2336e6fc85b831d3e3
MohanLi <lmh19920411@163.com>
2016-04-06 23:27:22 +0800

    mdf

--- Unity/MyRPG/Assets/Scripts/BackgroundController.cs
+++ Unity/MyRPG/Assets/Scripts/BackgroundController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class BackgroundController : MonoBehaviour {
     public Transform[] mBackground;
+    public float parallaScale;
+    public float smooting;
 
     private Transform mCamera;
     private Vector3 mPrePosition;
@@ -15,6 +17,24 @@ public class BackgroundController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float posX = mCamera.position.x - mPrePosition.x;
+        float parallax = (mCamera.position.x - mPrePosition.x) * parallaScale;
+
+        for (int i = 0; i < mBackground.Length; i++) {
+            float backgroundTargetPosX = mBackground[i].position.x + (i + 1) * parallax;
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, mBackground[i].position.y, mBackground[i].position.z);
+            mBackground[i].position = Vector3.Lerp(mBackground[i].position, backgroundTargetPos, smooting * Time.deltaTime);
+        }
+        mPrePosition = mCamera.position;
 	}
+
+    void FixedUpdate() {
+       // int unm = Random.Range(1, 3);
+       // Vector3 pos = new Vector3(mCamera.position.x +0.01f* Mathf.Pow(-1f, unm), mCamera.position.y, mCamera.position.z);
+       // mCamera.position = pos;// Vector3.Lerp(mCamera.position, pos, Time.deltaTime * 0.01f);
+        
+
+        float x = Input.GetAxis("Horizontal");
+        mCamera.position = new Vector3(x, mCamera.position.y, mCamera.position.z);
+        Debug.Log(mCamera.position);
+    }
 }
